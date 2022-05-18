b320152efe23758c01b023626e283a1d0ce70d70
antaww <anta.pizzetta@gmail.com>
2022-05-18 15:48:52 +0200

    timer working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let playBtn = document.querySelector("#play");
 let resetBtn = document.querySelector("#reset");
+let timer = document.querySelector(".timer");
 
 let canvas = {
     cvs: document.querySelector(".grid"),
@@ -7,32 +8,63 @@ let canvas = {
 }
 
 let isPlaying = false;
+let isClicking = false;
+
+let timerValue = 0;
+let currentDifficulty = 0; // 0 = not playing, 5 = easy, 6 = medium, 8 = hard
 
 
 playBtn.addEventListener("click", function (event) {
     console.clear();
+    playBtn.style.display = "none";
     isPlaying = true;
+    timer.style.display = "block";
+    currentDifficulty = 5;
+    startTimer();
     canvas.cvs.attributes.width.value = 400;
     canvas.cvs.attributes.height.value = 400;
     fillCanvas("#fff");
-    drawGrid("#000", 5, 5, 1);
+    drawGrid("#000", currentDifficulty, currentDifficulty, 1);
 
 });
 
-//event listener to mouse to get the position of the mouse
-canvas.cvs.addEventListener("mousemove", function (event) {
+window.addEventListener("mousedown", function (event) {
     if (isPlaying) {
-        let x = event.offsetX;
-        let y = event.offsetY;
-        getCurrentCell(x, y);
-        colorCell("#000")
+        isClicking = true;
+    }
+});
+
+window.addEventListener("mouseup", function (event) {
+    if (isPlaying) {
+        resetCells();
+        isClicking = false;
     }
 });
 
 
+canvas.cvs.addEventListener("mousemove", function (event) {
+    if (isClicking) {
+        if (isPlaying) {
+            let x = event.offsetX;
+            let y = event.offsetY;
+            getCurrentCell(x, y);
+            colorCell("#000")
+        }
+    }
+});
+
+//if the mouse is out of the canvas resetCells
+canvas.cvs.addEventListener("mouseout", function (event) {
+    if (isPlaying) {
+        resetCells();
+    }
+});
+
 resetBtn.addEventListener("click", function (event) {
     console.clear();
+    playBtn.style.display = "block";
     isPlaying = false;
+    stopTimer();
     canvas.cvs.attributes.width.value = 0;
     canvas.cvs.attributes.height.value = 0;
 });
@@ -41,14 +73,24 @@ resetBtn.addEventListener("click", function (event) {
 // UTILS //
 ///////////
 
-//fillCanvas function to fill the canvas with a color
+
+/**
+ * Fill the canvas with a color.
+ * @param color - The color to fill the canvas with.
+ */
 function fillCanvas(color) {
     canvas.ctx.fillStyle = color;
     canvas.ctx.fillRect(0, 0, canvas.cvs.width, canvas.cvs.height);
 }
 
-//drawGrid function to draw the grid on the canvas
-//parameters: color, number of rows, number of columns, border size
+
+/**
+ * It draws a grid on the canvas
+ * @param color - The color of the grid lines.
+ * @param rows - The number of rows in the grid.
+ * @param columns - The number of columns in the grid.
+ * @param borderSize - The size of the border of the grid.
+ */
 function drawGrid(color, rows, columns, borderSize) {
     canvas.ctx.strokeStyle = color;
     canvas.ctx.lineWidth = borderSize;
@@ -68,7 +110,13 @@ function drawGrid(color, rows, columns, borderSize) {
     }
 }
 
-//getCurrentCell function to get the current cell
+
+/**
+ * It takes in the x and y coordinates of the mouse click and returns the row and column of the cell that was clicked
+ * @param x - The x coordinate of the mouse click.
+ * @param y - The y-coordinate of the mouse pointer
+ * @returns The row and column of the cell that the user clicked on.
+ */
 function getCurrentCell(x, y) {
     let width = canvas.cvs.width / 5;
     let height = canvas.cvs.height / 5;
@@ -78,7 +126,11 @@ function getCurrentCell(x, y) {
     return [row + 1, column + 1];
 }
 
-//colorCell function to color the cell using getCurrentCell function
+
+/**
+ * It takes a color and fills the cell that the user clicked on with that color
+ * @param color - the color to fill the cell with
+ */
 function colorCell(color) {
     let cell = getCurrentCell(event.offsetX, event.offsetY);
     let width = canvas.cvs.width / 5;
@@ -87,4 +139,35 @@ function colorCell(color) {
     let column = cell[1] - 1;
     canvas.ctx.fillStyle = color;
     canvas.ctx.fillRect(column * width, row * height, width, height);
+}
+
+
+/**
+ * It resets the canvas to its original state
+ */
+function resetCells() {
+    fillCanvas("#fff");
+    drawGrid("#000", currentDifficulty, currentDifficulty, 1);
+}
+
+//startTimer displays the timer on the screen (minutes:seconds)
+function startTimer() {
+    timerValue = 0;
+    timer.innerHTML = "00:00";
+    timerInterval = setInterval(function () {
+        timerValue++;
+        let minutes = Math.floor(timerValue / 60);
+        let seconds = timerValue % 60;
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        timer.innerHTML = minutes + ":" + seconds;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
 }
