21d779f372fbdd5435a82d602943bc228bac8a87
antaww <anta.pizzetta@gmail.com>
2025-05-30 23:24:11 +0200

    chore(dependencies): Update package manager to pnpm@9.10.0 and switch Svelte adapter to adapter-auto.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,15 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined, // 'index.html' or '200.html' for SPAs, undefined for traditional sites
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		alias: {
 				// you can define custom aliases here
 				// '$lib': './src/lib',
